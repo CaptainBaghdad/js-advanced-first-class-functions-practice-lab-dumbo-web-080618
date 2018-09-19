5e37228d068010aeb03617570b28d67534b39218
Jason Pitts <juniorjr083@gmail.com>
2018-09-19 11:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(ele){
+    console.log(ele.name);
+    
+    
+  });
+  
+  
+}
+
+function logDriversByHometown(drivers, location){
+  drivers.forEach(function(ele){
+    if(ele.hometown == location){
+      console.log(ele.name);
+      
+    }
+    
+  });
+  
+}
+
+function driversByRevenue(drivers){
+
+  let ans =  drivers.slice().sort(function(ele1,ele2){
+    return ele1.revenue - ele2.revenue;
+    
+    
+    
+  });
+  
+  return ans;
+  
+}
